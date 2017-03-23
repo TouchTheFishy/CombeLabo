42662bcbf8c559254c42c26b08c908c0aa15dc9b
Paolo <paolodekeyzer@gmail.com>
2017-03-23 15:39:18 +0100

    zoeizefze

--- AsianNotAsian.py
+++ AsianNotAsian.py
@@ -4,16 +4,18 @@ import time
 class AsianNotAsian():
     def __init__(self,input):
         self.input=input
+        PWM.start("P9_14", 90)
 
 
     def IsAsian(self):
-        PWM.start("P9_14", 0)
-        PWM.set_duty_cycle("P9_14", 90)
+
         for i in range(0, 100):
             PWM.set_duty_cycle("P9_14", float(i))
             time.sleep(1)
         PWM.stop("P9_14")
         PWM.cleanup()
 
+
 obj=AsianNotAsian(155)
 obj.IsAsian()
+print("ergezr")
