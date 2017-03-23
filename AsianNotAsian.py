8053cea0263c04c37891dab649c6e24a9c8faa92
Paolo <paolodekeyzer@gmail.com>
2017-03-23 16:10:24 +0100

    zoeizefzejuygjgyfoij

--- AsianNotAsian.py
+++ AsianNotAsian.py
@@ -9,7 +9,7 @@ class AsianNotAsian():
 
     def IsAsian(self):
 
-        PWM.set_duty_cycle("P9_14", 50)
+        PWM.set_duty_cycle("P9_14", 100)
         time.sleep(1)
         PWM.stop("P9_14")
         PWM.cleanup()
