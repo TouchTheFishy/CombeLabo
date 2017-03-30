f0361ba6a74ae7c2ad0ff9e9ca1525778cfba64c
MisterTarock <victor_pb@hotmail.fr>
2017-03-30 10:08:05 +0200

    str for pin in gpio

--- gpio.py
+++ gpio.py
@@ -8,10 +8,10 @@ def GPIO(pin, port):
     Chose a port, the port 9 is 1 and the port 8 is 2.
     """
 
-    Write("/sys/class/gpio/export", 32 * port + pin)
+    Write("/sys/class/gpio/export", str(32 * port + pin))
 # On peut toujours tapper une exception s'il mets un autre port ou une autre pin et n'importe quoi d'autre
 
     Write("/sys/class/gpio/direction", "out")
     Write("/sys/class/gpio/value", 1)
-    Write("/sys/class/gpio/unexport", 32 * port +pin)
+    Write("/sys/class/gpio/unexport", str(32 * port +pin))
 
