11f1a4521dd4b8656afbb4e874db49ba9efd8e73
Mathieu David <mathieudavid@mathieudavid.org>
2017-03-30 09:21:24 +0200

    I2C bus handling

--- rgbsensor.py
+++ rgbsensor.py
@@ -1,3 +1,7 @@
+import os
+from fcntl import ioctl
+
+
 class RGBSensor():
     """docstring for RGBSensor."""
 
@@ -5,13 +9,9 @@ class RGBSensor():
         self.adress = 0x29
         self.int_pin = int_pin
 
-        with open("/dev/i2c-2", "rb+") as i2c:
-            i2c.write(bytes(self.adress << 1 | 1))
-            i2c.write(bytes(0x00))
-            i2c.write(bytes(0b00000011))
+        self.i2c = open("/dev/i2c-2", "rb+")
+        ioctl(self.i2c, 0x703 self.adress)
 
     def get_color():
-        with open("/dev/i2c-2", "rb+") as i2c:
-            i2c.write(bytes(self.adress << 1) + bytes(0x14))
-            data = i2c.read(8)
-            print(data)
+        data = os.read(self.i2c, 8)
+        print(data)
