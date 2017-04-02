cc2e2422b2805990415ee1aa172bf6359befb3a0
Mathieu David <mathieudavid@mathieudavid.org>
2017-04-02 15:02:04 +0200

    Reorganize gpio code

--- gpio.py
+++ gpio.py
@@ -1,17 +1,18 @@
 import os
 
-def Write(chemin, val1):
-    with open(chemin, "w") as f:
-        f.write(val1)
-
 def GPIO(pin, port):
     """
     Chose a pin between 1 and 46
     Chose a port, the port 9 is 1 and the port 8 is 2.
     """
 
+    # Internal function to make code DRY
+    def write_gpio(path, value):
+        with open(path, "w") as f:
+            f.write(value)
+
     if not os.path.exists("/sys/class/gpio/export"):
-        Write("/sys/class/gpio/export", str(32 * port + pin))
-        Write("/sys/class/gpio/direction", "out")
-        Write("/sys/class/gpio/value", 1)
-        Write("/sys/class/gpio/unexport", str(32 * port + pin))
+        write_gpio("/sys/class/gpio/export", str(32 * port + pin))
+        write_gpio("/sys/class/gpio/direction", "out")
+        write_gpio("/sys/class/gpio/value", 1)
+        write_gpio("/sys/class/gpio/unexport", str(32 * port + pin))
