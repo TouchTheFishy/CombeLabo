699da7a9dd1facbc1569f6dfc68b5bb3c664dae9
Mathieu David <mathieudavid@mathieudavid.org>
2017-04-02 14:55:55 +0200

    Add comments

--- accellerometer.py
+++ accellerometer.py
@@ -3,20 +3,36 @@ from fcntl import ioctl
 
 
 class Accellerometer():
-    """Class abstracting the MMA7455L Accellerometer"""
+    """
+    Class abstracting the MMA7455L Accellerometer
+    providing methods to retrieve the x, y and z acceleration components
+    """
 
     def __init__(self):
+        """initiates the communication on the I2C bus with the accellerometer"""
         self.adress = 0x1D
-
         self.i2c = os.open("/dev/i2c-2", os.O_RDWR)
         ioctl(self.i2c, 0x703, self.adress)
 
     def get_x(self):
+        """get the x component of the acceleration"""
         os.write(self.i2c, bytes([0x00]))
         data = os.read(self.i2c, 2)
         return data[0] & (data[1] << 8)
 
+    def get_y(self):
+        """get the y component of the acceleration"""
+        os.write(self.i2c, bytes([0x02]))
+        data = os.read(self.i2c, 2)
+        return data[0] & (data[1] << 8)
 
+    def get_z(self):
+        """get the z component of the acceleration"""
+        os.write(self.i2c, bytes([0x04]))
+        data = os.read(self.i2c, 2)
+        return data[0] & (data[1] << 8)
+
+# Test
 if __name__ == '__main__':
     a = Accellerometer()
     print(a.get_x())
